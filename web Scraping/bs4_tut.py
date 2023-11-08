bbfae3670a0660d0296807d83c0966d7fc23743a
e21cseu0252@bennett.edu.in <113098477+SAI-EASWAR@users.noreply.github.com>
2023-11-08 19:56:13 +0530

    changed->some more info

--- web Scraping/bs4_tut.py
+++ web Scraping/bs4_tut.py
@@ -13,7 +13,18 @@ soup = BeautifulSoup(html_doc,"html.parser")
 
 # we can also use css selectors in this
 
-xc=soup.select("a")
-print(xc)
+# xc=soup.select("a")
+# print(xc)
+
+#how to change the content in a html tag
+# x = soup.find(class_="myclass")
+# x.string= "this has been changed"
+# print(x)
+
+
+#to get the parent of the give tag
+# x = soup.find(class_="myclass")
+# print(x.parent)
+
 
 
