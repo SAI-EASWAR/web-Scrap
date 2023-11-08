97770f45c9a24531190efe3ded9fcd1b60a1acbd
e21cseu0252@bennett.edu.in <113098477+SAI-EASWAR@users.noreply.github.com>
2023-11-08 19:47:29 +0530

    channge

--- web Scraping/bs4_tut.py
+++ web Scraping/bs4_tut.py
@@ -1,7 +1,19 @@
 from bs4 import BeautifulSoup
 
-
 with open("web Scraping/basics_bs4.html","r") as f:
     html_doc = f.read() 
 soup = BeautifulSoup(html_doc,"html.parser")
-print(soup.prettify())
+
+# get the html code
+# print(soup.prettify())
+
+# to find all the tag with tag div
+# for divs in soup.find_all("div"):
+#     print(divs)
+
+# we can also use css selectors in this
+
+xc=soup.select("a")
+print(xc)
+
+
